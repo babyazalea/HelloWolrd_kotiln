4ac8f37964ae0dd841c19991e7073a13ed3e720d
babyazalea <usun12@hotmail.com>
2020-11-19 17:43:33 +0900

    first commit

---
+++ app/src/main/java/com/babyazalea/helloworld/KotlinBasics.kt
@@ -0,0 +1,36 @@
+package com.babyazalea.helloworld
+
+fun main(){
+//    immutable variable
+//    TODO: Add new fuctionallity
+
+//    type String
+    val myName = "Taeyang"
+//    type int 32 bit
+    var myAge = 31
+
+//    Data Types
+    val myByte: Byte = 12
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 39_812_309_487_120_300
+
+//    Floating Point number Types: Float (32 bit), Double (64 bit)
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.123124123123
+
+//    Booleans he type Boolean is used to represent logical values.
+//    It can have two possible values true and false.
+    var isSunny: Boolean = true
+    isSunny = false
+
+//    Characters
+    val letterChar = "A"
+    val digitChar = "1"
+
+//    Strings
+    val myStr = "Hello World"
+    var firstCharInStr = myStr[0]
+
+    print("hello " + myName)
+}
