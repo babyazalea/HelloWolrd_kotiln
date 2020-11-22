9398ba561ed54f04bbb109890838d7ee7a57b612
babyazalea <usun12@hotmail.com>
2020-11-22 21:17:21 +0900

    if statement

--- app/src/main/java/com/babyazalea/helloworld/KotlinBasics.kt
+++ app/src/main/java/com/babyazalea/helloworld/KotlinBasics.kt
@@ -47,24 +47,100 @@ fun main(){
 
     val isNotEqual = 5!=5
 //    String interpolation
-    println("isNotEqual is $isNotEqual")
+    // println("isNotEqual is $isNotEqual")
 
-    println("is5greater3 ${-5>3}")
-    println("in5LowerEqua13 ${5 <= 3}")
-    println("in5GreaterEqua13 ${5 >= 3}")
+    // println("is5greater3 ${-5>3}")
+    // println("in5LowerEqua13 ${5 <= 3}")
+    // println("in5GreaterEqua13 ${5 >= 3}")
 
 //    Assingment operators(+=, -=,*=, /=, %=)
     var myNum = 5
     myNum += 3
     myNum *= 4
-    println("myNum is $myNum")
+    // println("myNum is $myNum")
 
 //    Increment & Decrement  Operators(++, --)
     myNum++
-    println("myNum is $myNum")
+    // println("myNum is $myNum")
 //    below line is excuted but nothing change beacause JUST EXCUTED, not apply.
-    println("myNum is ${myNum++}")
+    // println("myNum is ${myNum++}")
 //    below line in excuted, and then apply immediately.
-    println("myNum is ${++myNum}")
-    println("myNum is ${--myNum}")
+    // println("myNum is ${++myNum}")
+    // println("myNum is ${--myNum}")
+
+    var heightPerson1 = 170
+    var heightPerson2 = 189
+
+    if(heightPerson1 > heightPerson2){
+        println("use raw force")
+    }else if(heightPerson1 ==  heightPerson2){
+        println("use your power technique 1337")
+    }else{
+        println("use technique")
+    }
+
+    val age = 17
+   if(age >= 21) {
+            println("now you may drink in the US")
+        } else if (age >= 18) {
+            println("you may vote now")
+        }
+        else if(age >= 16) {"Live now"
+        }
+        else {
+            println("you're too young")
+        }
+
+
+    var name = "ty"
+
+    if(name == "ty"){
+        println("welcome home")
+    } else{
+        println("who are you")
+    }
+
+    val isRainy = true
+
+    if(isRainy){
+        println("It's rainy")
+    }
+
+    var season = 3
+
+    when(season){
+         1 -> println("Spring")
+         2 -> println("summer")
+         3 -> {
+             println("Fall")
+             println("Autumn")
+         }
+         4 -> {
+             println("winter")
+         }
+    }
+
+    var month = 3
+    when(month){
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        in 12 downTo 2 -> println("Winter")
+        else -> println("Invalid Season")
+    }
+
+    when(age){
+        !in 21..150 -> println("now you may drink in the US")
+        in 18..20 -> println("you may vote now")
+        16,17 -> println("you may drive now")
+        else -> println("you're too young")
+    }
+
+    var x : Any = 13.37f
+    when(x){
+        is Int -> println("$x is an Int")
+        !is Double -> println("$x is not a Double")
+        is String -> println("$x is an String")
+        else -> println("$x is none of the above")
+    }
 }
