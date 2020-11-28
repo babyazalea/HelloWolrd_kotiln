19efe782509ae3a59af84aa73d0ed5a989cbe044
babyazalea <usun12@hotmail.com>
2020-11-28 11:56:38 +0900

    Elvis operator

--- app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
+++ app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
@@ -1,14 +1,20 @@
 package com.babyazalea.helloworld
 
 fun main(){
-    var name: String = "taeyang"
+    var name1: String = "taeyang"
     // name = null -> Compilation ERROR
     var nullableName : String? = "Taeyang"
     //nullableName = null
 
-    var len = name.length
+//    var len = name.length
     var len2 = nullableName?.length
-    nullableName?.let { println(it.length) }
+//    nullableName?.let { println(it.length) }
+
+    // ?: Elvis operator
+    val name = nullableName ?: "Guest"
+    println("name is $name")
+
+    println(nullableName!!.toLowerCase())
 
     /*
     if(nullableName != null){
