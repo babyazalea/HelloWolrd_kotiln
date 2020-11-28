a3b7ebcbb75c521e9eb473bf22932780f6b1a50a
babyazalea <usun12@hotmail.com>
2020-11-28 13:58:21 +0900

    Classes and Initializers

--- app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
+++ app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
@@ -1,28 +1,14 @@
 package com.babyazalea.helloworld
 
 fun main(){
-    var name1: String = "taeyang"
-    // name = null -> Compilation ERROR
-    var nullableName : String? = "Taeyang"
-    //nullableName = null
+    var ty = Person("Taeyang", "Yu")
+    var em = Person()
+    var johnPeterson = Person(lastName = "Perterson")
+}
 
-//    var len = name.length
-    var len2 = nullableName?.length
-//    nullableName?.let { println(it.length) }
-
-    // ?: Elvis operator
-    val name = nullableName ?: "Guest"
-    println("name is $name")
-
-    println(nullableName!!.toLowerCase())
-
-    /*
-    if(nullableName != null){
-        var len2 = nullableName.length
-    }else{
-        null
+class Person  constructor(firstName: String = "Eunmin", lastName: String = "Choi"){
+    // Initializer Block
+    init {
+        println("Initialized a new Person Object with firstName = $firstName and lastName = $lastName")
     }
-    */
-
-
 }
