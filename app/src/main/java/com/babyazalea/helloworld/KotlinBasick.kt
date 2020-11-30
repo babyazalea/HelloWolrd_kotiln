855494a1a0b96c9093f4dec9a452e6be2539da05
babyazalea <usun12@hotmail.com>
2020-11-30 20:32:27 +0900

    Class Members(Constructor & Init & Properties & Function)

--- app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
+++ app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
@@ -1,12 +1,37 @@
 package com.babyazalea.helloworld
 
 fun main(){
-    myFunction(5)
+    // Creating instance of classes
+    var ty = Person("Taeyang", "Yu", 31)
+    ty.hobby = "play game"
+    ty.stateHobby() // output: My hobby is game
+    var em = Person() // output: Initialized a new Person object withfirstName = eunmin and lastName = choi
+    em.hobby = "writing kkamzi"
+    em.stateHobby()
+
 }
 // this a is a parameter
-fun myFunction(a:Int) {
-    // a is a variable
-    var a = a
-    println("a is $a")
-}
+class Person(firstName: String = "eunmin", lastName: String ="choi"){
+    // Member Variables - Properties
+    var age : Int? = null
+    var hobby : String? = null
+    var firstName : String? = null
 
+    //Initializer Block
+    init {
+        this.firstName = firstName
+        println("Initialized a new Person object with" + "firstName = $firstName and lastName = $lastName ")
+    }
+
+    // Member secondary Constructor
+    constructor(firstName: String, lastName: String, age: Int)
+            : this(firstName, lastName){
+                this.age = age
+                println("Initialized a new Person object with" + "firstName = $firstName and lastName = $lastName and $age ")
+            }
+
+    // Member Functions - Methods
+    fun stateHobby(){
+        println("$firstName hobby is $hobby")
+    }
+}
