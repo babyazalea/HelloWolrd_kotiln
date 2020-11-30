1a200ba684d20af8153c2957873f692b6547d6e5
babyazalea <usun12@hotmail.com>
2020-11-30 18:32:55 +0900

    Scope and Shadowing

--- app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
+++ app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
@@ -1,14 +1,12 @@
 package com.babyazalea.helloworld
 
 fun main(){
-    var ty = Person("Taeyang", "Yu")
-    var em = Person()
-    var johnPeterson = Person(lastName = "Perterson")
+    myFunction(5)
+}
+// this a is a parameter
+fun myFunction(a:Int) {
+    // a is a variable
+    var a = a
+    println("a is $a")
 }
 
-class Person  constructor(firstName: String = "Eunmin", lastName: String = "Choi"){
-    // Initializer Block
-    init {
-        println("Initialized a new Person Object with firstName = $firstName and lastName = $lastName")
-    }
-}
