0532dcbb4f73b8b28bdabe3e02370065b12bcece
babyazalea <usun12@hotmail.com>
2020-11-30 21:06:35 +0900

    Lateinit, Setter and Getter

--- app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
+++ app/src/main/java/com/babyazalea/helloworld/KotlinBasick.kt
@@ -1,37 +1,35 @@
 package com.babyazalea.helloworld
 
+import java.lang.IllegalArgumentException
+
 fun main(){
-    // Creating instance of classes
-    var ty = Person("Taeyang", "Yu", 31)
-    ty.hobby = "play game"
-    ty.stateHobby() // output: My hobby is game
-    var em = Person() // output: Initialized a new Person object withfirstName = eunmin and lastName = choi
-    em.hobby = "writing kkamzi"
-    em.stateHobby()
-
+    var myCar = Car()
+    println("brand is : ${myCar.myBrand}") // this is 'get'
+    myCar.maxSpeed = 240 // this is 'set'
+    println("Maxspeed is ${myCar.maxSpeed}") // this is 'get'
+    println("My model is ${myCar.myModel}") // this is 'get'
 }
-// this a is a parameter
-class Person(firstName: String = "eunmin", lastName: String ="choi"){
-    // Member Variables - Properties
-    var age : Int? = null
-    var hobby : String? = null
-    var firstName : String? = null
 
-    //Initializer Block
+class Car(){
+    lateinit var owner: String
+
+    val myBrand: String = "BMW"
+        // Custom getter
+        get() {
+            return field.toLowerCase()
+        }
+
+    var maxSpeed: Int = 250
+        // get() = field
+        set(value){
+            field = if(value > 0) value else throw IllegalArgumentException("MaxSpeed cannot be less than 0")
+        }
+
+    var myModel : String = "M5"
+        private set
+
     init {
-        this.firstName = firstName
-        println("Initialized a new Person object with" + "firstName = $firstName and lastName = $lastName ")
-    }
-
-    // Member secondary Constructor
-    constructor(firstName: String, lastName: String, age: Int)
-            : this(firstName, lastName){
-                this.age = age
-                println("Initialized a new Person object with" + "firstName = $firstName and lastName = $lastName and $age ")
-            }
-
-    // Member Functions - Methods
-    fun stateHobby(){
-        println("$firstName hobby is $hobby")
+        this.myModel = "M3"
+        owner = "Frank"
     }
 }
